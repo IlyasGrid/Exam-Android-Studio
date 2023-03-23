ebc6da209bb81383ab97a1e36e85aa52c4b3a055
Admin <Admin@DESKTOP-IOMF4D2>
2023-03-23 15:57:19 +0000

    f

--- app/src/main/java/com/example/exam/RegisterActivity.java
+++ app/src/main/java/com/example/exam/RegisterActivity.java
@@ -3,8 +3,35 @@ package com.example.exam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private EditText confirmpwd
+    private Button registerBtn;
+    private EditText phone;
+
+    public EditText getEmail() {
+        return email;
+    }
+
+    public EditText getPassword() {
+        return password;
+    }
+
+    public EditText getConfirmpwd() {
+        return confirmpwd;
+    }
+
+    public Button getRegisterBtn() {
+        return registerBtn;
+    }
+
+    public EditText getPhone() {
+        return phone;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/exam/actions/Register.java
+++ app/src/main/java/com/example/exam/actions/Register.java
@@ -1,4 +1,22 @@
 package com.example.exam.actions;
 
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.exam.MainActivity;
+import com.example.exam.RegisterActivity;
+
 public class Register {
+    private RegisterActivity registerActivity;
+    private EditText email;
+    private EditText password;
+    private EditText confirmpwd
+    private Button registerBtn;
+    private EditText phone;
+    public Register(RegisterActivity registerActivity){
+        this.registerActivity =registerActivity;
+        email = registerActivity.getEmail();
+        password = registerActivity.getPwd();
+        registerActivity = registerActivity.getRegisterbtn();
+    }
 }
